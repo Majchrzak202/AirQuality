4ca4158900969f895fdf9c56ff46e0a3fcce5b02
MarcinMaster <m.luba505@gmail.com>
2022-10-24 16:14:51 +0200

    Added images

---
+++ src/assets/IndicatorItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card, Typography,  } from '@mui/material'
+
+const IndicatorItem = () => {
+  return (
+    <Card>
+
+    </Card>
+  )
+}
+
+export default IndicatorItem
