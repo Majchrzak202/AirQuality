bea1647b4ffde5ee6fdd7bfae4b49494ab08d8be
MarcinMaster <m.luba505@gmail.com>
2022-10-24 16:15:23 +0200

    Added pre-defined memos for the Recomendation Component

---
+++ src/components/TextArray.js
@@ -0,0 +1,153 @@
+import { Masks } from "@mui/icons-material";
+import { PedalBike } from "@mui/icons-material";
+import { Air } from "@mui/icons-material";
+import { Window } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+
+export const greenArray = [
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Open windows and bring clean, fresh air
+      </Typography>
+    ),
+    icon: <Window style={{ width: 70, height: 70, color: "#12cd7c" }} />,
+    id: 1
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Air Purifier is not needed"
+      </Typography>
+    ),
+    icon: <Air style={{ width: 70, height: 70, color: "#12cd7c" }} />,
+    id: 2
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Enjoy outdoor activities
+      </Typography>
+    ),
+    icon: <PedalBike style={{ width: 70, height: 70, color: "#12cd7c" }} />,
+    id: 3
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>You dont need mask</Typography>
+    ),
+    icon: <Masks style={{ width: 70, height: 70, color: "#12cd7c" }} />,
+    id: 4
+  },
+];
+
+export const yellowArray = [
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Close your windows to avoid dirty outdoor air
+      </Typography>
+    ),
+    icon: <Window style={{ width: 70, height: 70, color: "#f2bf30" }} />,
+    id: 1
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Air Purifier is not needed"
+      </Typography>
+    ),
+    icon: <Air style={{ width: 70, height: 70, color: "#f2bf30" }} />,
+    id: 2
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Sensitive groups should reduce outdoor exercise
+      </Typography>
+    ),
+    icon: <PedalBike style={{ width: 70, height: 70, color: "#f2bf30" }} />,
+    id: 3
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>You dont need mask</Typography>
+    ),
+    icon: <Masks style={{ width: 70, height: 70, color: "#f2bf30" }} />,
+    id: 4
+  },
+];
+
+export const orangeArray = [
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Close your windows to avoid dirty outdoor air
+      </Typography>
+    ),
+    icon: <Window style={{ width: 70, height: 70, color: "#ff9b57" }} />,
+    id: 1
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>Run an air purifier</Typography>
+    ),
+    icon: <Air style={{ width: 70, height: 70, color: "#ff9b57" }} />,
+    id: 2
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Everyone should reduce outdoor exercise
+      </Typography>
+    ),
+    icon: <PedalBike style={{ width: 70, height: 70, color: "#ff9b57" }} />,
+    id: 3
+  },
+  {
+    text: (
+      <Typography style={{ paddingLeft: 5 }}>
+        Sensitive groups should wear a mask outdoors
+      </Typography>
+    ),
+    icon: <Masks style={{ width: 70, height: 70, color: "#ff9b57" }} />,
+    id: 4
+  },
+];
+
+export const redArray = [
+    {
+        text: (
+          <Typography style={{ paddingLeft: 5 }}>
+            Close your windows to avoid dirty outdoor air
+          </Typography>
+        ),
+        icon: <Window style={{ width: 70, height: 70, color: "#f65f60" }} />,
+        id: 1
+      },
+      {
+        text: (
+          <Typography style={{ paddingLeft: 5 }}>Run an air purifier</Typography>
+        ),
+        icon: <Air style={{ width: 70, height: 70, color: "#f65f60" }} />,
+        id: 2
+      },
+      {
+        text: (
+          <Typography style={{ paddingLeft: 5 }}>
+           Avoid outdoor exercise
+          </Typography>
+        ),
+        icon: <PedalBike style={{ width: 70, height: 70, color: "#f65f60" }} />,
+        id: 3
+      },
+      {
+        text: (
+          <Typography style={{ paddingLeft: 5 }}>
+            	Wear a mask outdoors
+          </Typography>
+        ),
+        icon: <Masks style={{ width: 70, height: 70, color: "#f65f60" }} />,
+        id: 4
+      },
+];
