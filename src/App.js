4ff1550cfeb15f80b84e07c77df50c1c6b36a47d
MarcinMaster <m.luba505@gmail.com>
2022-10-28 07:46:21 +0200

    Added footer component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,29 @@ import { Routes, Route } from "react-router";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import MoreInfo from "./components/MoreInfo";
+import PageNotFound from "./components/PageNotFound";
+import Footer from "./components/Footer";
 import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 
-const custom_theme = createTheme({
+export const custom_theme = createTheme({
   palette: {
     primary: {
-      main: '#80A3FF',
-      contrastText: "#fff"
-    }, 
+      main: "#12cd7c",
+      contrastText: "#fff",
+    },
     secondary: {
-      main: '#F5B041',
-      contrastText: "#fff"
-    }
+      main: "#15EA23",
+      contrastText: "#fff",
+    },
+    warning: {
+      main: "#0da418",
+    },
   },
   typography: {
-     fontFamily: 'Ubuntu' 
-  }
+    fontFamily: "Ubuntu",
+  },
 });
 
 const App = () => {
@@ -32,7 +38,10 @@ const App = () => {
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/more-info" element={<MoreInfo />}></Route>
+          <Route path='*' element={<PageNotFound/>}/>
         </Routes>
+        <Footer/>
       </ThemeProvider>
     </>
   );
