5fa8546d19947e583e8e5d3d719b208586ce1c93
MarcinMaster <m.luba505@gmail.com>
2022-10-28 07:49:13 +0200

    Switched color of hover links in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,23 +5,63 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  navbarLink: {
+    margin: 10,
+    color: "white",
+    "&:hover": {
+      color: "#0e5158",
+    },
+    textDecoration: "none",
+    cursor: "pointer",
+    padding: 10,
+  },
+});
 
 const Navbar = () => {
+  const classes = useStyles();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar color='primary' position="static">
+      <AppBar color="primary" position="static">
         <Toolbar>
-          <Typography variant="h6" component="div">
-            <Link to='/'>
-            Homepage
-            </Link>
-          </Typography>
-          <Typography variant="h6" component="div">
-            About
-          </Typography>
-          <Typography variant="h6" component="div">
-            More info
-          </Typography>
+          <Link style={{ textDecoration: "none" }} to="/">
+            <Typography
+              className={classes.navbarLink}
+              variant="h6"
+              component="div"
+            >
+              Homepage
+            </Typography>
+          </Link>
+          <Link style={{ textDecoration: "none" }} to="/about">
+            <Typography
+              className={classes.navbarLink}
+              variant="h6"
+              component="div"
+            >
+              About
+            </Typography>
+          </Link>
+          <Link style={{ textDecoration: "none" }} to="/more-info">
+            <Typography
+              className={classes.navbarLink}
+              variant="h6"
+              component="div"
+            >
+              More info
+            </Typography>
+          </Link>
+          <Link style={{ textDecoration: "none" }} to="/contact">
+            <Typography
+              className={classes.navbarLink}
+              variant="h6"
+              component="div"
+            >
+              Contact
+            </Typography>
+          </Link>
           <Box sx={{ flexGrow: 1 }} />
           <Button color="inherit">Login</Button>
         </Toolbar>
