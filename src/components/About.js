33d6d39d86fb005065eb40444c83e48c0063a42f
MarcinMaster <m.luba505@gmail.com>
2022-10-28 07:50:08 +0200

    Added cool background Image and fonts into About component. Moved it into Home tree

--- src/components/About.js
+++ src/components/About.js
@@ -1,43 +1,95 @@
 import React from "react";
 import { Box } from "@mui/system";
 import BackgroundJpg from "./../assets/AboutBackground.png";
-import { Paper } from "@mui/material";
+import { Divider, Typography } from "@mui/material";
+import { Link as SmoothLink } from "react-scroll";
+import { makeStyles } from "@mui/styles";
 
-const styles = {
-  paperContainer: {
-    backgroundImage: BackgroundJpg,
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "#f7faff",
   },
-};
+  img: {
+    height: "80%",
+    width: "100vw",
+  },
+  mainBox: {
+    display: "flex",
+    minHeight: "30vh",
+    alignItems: "flex-start",
+    backgroundColor: "#0f5157",
+    marginTop: -5,
+    flexDirection: "column",
+  },
+  titleBox: {
+    display: "flex",
+    width: 350,
+    height: "auto",
+    padding: 30,
+    margin: 10,
+    marginTop: -150,
+    color: "white",
+    flexDirection: "column",
+  },
+  divider: {
+    fontSize: 30,
+    fontWeight: 800,
+  },
+  linksBox: {
+    paddingTop: 20,
+    display: "flex",
+    justifyContent: "space-evenly",
+    color: "white",
+    minWidth: "100%",
+  },
+  link: {
+    cursor: "pointer",
+    "&:hover": {
+      color: "#f2bf30",
+    },
+  },
+});
 
 const About = () => {
+  const classes = useStyles();
   return (
-    <Paper style={styles.paperContainer}>
-      <img src={BackgroundJpg} style={{ height: "80%", width: "100vw" }} />
-      <Box
-        style={{
-          display: "flex",
-          minHeight: "50vh",
-          alignItems: 'flex-start',
-          backgroundColor: "#0f5157",
-          marginTop: -5,
-        }}
-      >
-        <Box
-          style={{
-            backgroundColor: "red",
-            display: "flex",
-            width: "auto",
-            height: "auto",
-            padding: 30,
-            margin: 10,
-            marginTop: -150,
-            color: 'white'
-          }}
-        >
-          Papa
+    <div className={classes.root}>
+      <img src={BackgroundJpg} className={classes.img} alt="about" />
+      <Box className={classes.mainBox}>
+        <Box className={classes.titleBox}>
+          <Typography style={{ fontSize: 30, fontWeight: 800 }}>
+            Change starts with you!
+          </Typography>
+          <Divider style={{ height: 3, backgroundColor: "#f2bf30" }} />
+          <Typography style={{ fontSize: 15, fontWeight: 600, paddingTop: 15 }}>
+            This is an Educational Project build in React! Feel free to contact
+            me! Site takes data from over 1600 stations to provide Air Quality
+            Data and weather conditions info.
+          </Typography>
+        </Box>
+
+        <Box className={classes.linksBox}>
+          <SmoothLink
+            to="search"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={700}
+            className={classes.link}
+          >
+            <Typography style={{ fontSize: 18, fontWeight: 800 }}>
+              Check Air Quality
+            </Typography>
+          </SmoothLink>
+          <Typography style={{ fontSize: 18, fontWeight: 800 }}>
+            Read about AQI
+          </Typography>
+          <Typography style={{ fontSize: 18, fontWeight: 800 }}>
+            Contact author
+          </Typography>
         </Box>
       </Box>
-    </Paper>
+    </div>
   );
 };
 
